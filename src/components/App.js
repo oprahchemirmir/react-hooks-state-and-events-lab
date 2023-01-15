2b2b4599174e038870fefdeb38d67a54aa05eec8
oprah <oprah.chemirmir@student.moringaschool.com>
2023-01-15 18:43:15 +0300

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
-function App() {
 
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+function App() {
+  const [app, setApp] = useState(false);
+
+  function handleClick() {
+    setApp(app => !app);
+  }
+  const appClass = app ? "App dark" : "App light"
 
   return (
     <div className={appClass}>
-      <header>
-        <h2>Shopster</h2>
-        <button>Dark Mode</button>
-      </header>
-      <ShoppingList items={itemData} />
-    </div>
+        <header>
+          <h2>Shopster</h2>
+          <button onClick={handleClick}>{app ? "Light Mode" : "Dark Mode"}</button>
+        </header>
+        <ShoppingList items={itemData} />
+      </div>
   );
 }
+
 
 export default App;
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,20 @@
-import React from "react";
+
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setselectedCategory] = useState(items);
+
+  function handleChange(e){
+    const cart = e.target.value;
+    const cartItems = items.filter((item)=> item.category === cart)
+        setselectedCategory(cartItems)
+  }
+ 
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,12 +22,13 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+      {selectedCategory.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
     </div>
   );
 }
+
 
 export default ShoppingList;
